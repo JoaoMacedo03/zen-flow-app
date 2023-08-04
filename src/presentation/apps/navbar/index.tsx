7734f15ec923b1a4c1fe89565ae5c76e832ab206
Joao Macedo <joaovictormacedo91@gmail.com>
2023-08-04 18:21:26 -0300

    feat: starting tests

--- src/presentation/apps/navbar/index.tsx
+++ src/presentation/apps/navbar/index.tsx
@@ -1,8 +1,95 @@
-import { BaseComponentProps } from '@/interfaces';
 import React from 'react';
+import ReactFlow, {
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from 'reactflow';
+import { BaseComponentProps } from '@/interfaces';
+import 'reactflow/dist/style.css';
+import './navbar.scss';
+import RecipeReviewCard from './test';
 
-const Navbar: React.FC<BaseComponentProps> = (_: BaseComponentProps) => (
-  <div data-testid="navbarWrap">Isso é um navbar</div>
-);
+interface NodeProps {
+  id: string;
+  position: { x: number; y: number };
+  data: { label: string; updateNode: any };
+  type?: string;
+}
+
+  const initialNodes: NodeProps[] = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1', updateNode: () => '' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2', updateNode: () => '' } },
+  ];
+  
+  const initialEdges = [{ id: 'dfkjdfglkdfjglkjkj', source: '1', target: '3' }];
+
+const Navbar: React.FC<BaseComponentProps> = (_: BaseComponentProps) => {
+  const nodeTypes = React.useMemo(() => ({ recipe: RecipeReviewCard }), []);  
+  
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const nodesRefs = React.useRef<NodeProps[]>(nodes);
+
+  const onConnect = React.useCallback(
+    (params: any) => {
+      setEdges((eds) => addEdge(params, eds))
+    }, [setEdges]
+  );
+
+  const test = React.useCallback((index: string, value: string) => {
+    setNodes((nds: any) => {
+      const aux = nds.map((node: any) => {
+        if (node.id === index) {
+          node.data = {
+            ...node.data,
+            label: value,
+          };
+        }
+  
+        return node;
+      });
+      return aux;
+    });    
+  }, [setNodes])
+
+  React.useEffect(() => {
+    nodesRefs.current = nodes;
+  }, [nodes])
+
+  const addNode = () => {
+    const aux = [...nodesRefs.current]
+    aux.push({ 
+      id: (nodes.length + 1).toString(), 
+      data: { label: 'Node', updateNode: test }, 
+      type: 'recipe',
+      position: { x: 0, y: 0 } 
+    })
+    setNodes(aux)
+  }
+
+  
+  return (
+    <div className="navbarWrap">
+      <div>
+        <> {JSON.stringify(nodes)}</>
+        
+      </div>
+      <button onClick={addNode}>Node</button>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </div>
+  )
+};
 
 export default Navbar;
